716684b1f123f7c9048b83e06eac4574639572ef
hyen-sn <dohoangyenlhp2326@gmail.com>
2026-05-08 23:54:40 +0700

    Update script.js
    thêm bảo mật 1

--- script.js
+++ script.js
@@ -263,3 +263,25 @@ function initMusicPlayer() {
   restoreMusicState();
   updatePlayButton();
 }
+
+
+
+
+/* =========================
+   BẢO MẬT
+========================= */
+document.addEventListener("contextmenu", e => e.preventDefault());
+
+document.onkeydown = function(e){
+
+  if(e.keyCode == 123){
+    return false;
+  }
+
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+    return false;
+  }
+
+}
+
+<meta name="robots" content="noindex,nofollow">
