f3f3b7ccd831beab5ee82028c33af41efbc592a4
hyen-sn <dohoangyenlhp2326@gmail.com>
2026-05-12 23:54:23 +0700

    Update script.js
    Thêm hash

--- script.js
+++ script.js
@@ -282,5 +282,19 @@ document.onkeydown = function(e){
     return false;
   }
 
+  /* =========================
+   HASH
+========================= */
 }
 
+async function sha256(text) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(text.normalize("NFC"));
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  return hashArray
+    .map(byte => byte.toString(16).padStart(2, "0"))
+    .join("");
+}
